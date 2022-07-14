89573c8e858c4398dcc7a2d1ef3f601cb9ea6344
alhosary <ahmedalhosary67@gmail.com>
2022-07-14 10:20:45 +0200

    update

--- src/pages/properties/index.js
+++ src/pages/properties/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { Button, Container, Form, FormControl, Row } from "react-bootstrap";
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import "./main.css";
+import { Avatar, Card } from "antd";
+import { EditOutlined, SettingsOutlined } from "@mui/icons-material";
+
+const { Meta } = Card;
 
 const Properies = () => {
   return (
@@ -17,8 +21,38 @@ const Properies = () => {
             />
             <Button variant="outline-success">Search</Button>
           </Form>
-          <h4 className="titleOfFilter"><ArrowRightIcon color="warning" fontSize="large"/> Properties For sale</h4>
-          <samll>Best properties For sale in , compare with prices and photos between <span className="counter">211,224</span> properties of different areas. Search in using the map and contact the seller directly.</samll>
+          <h4 className="titleOfFilter">
+            <ArrowRightIcon color="warning" fontSize="large" /> Properties For
+            sale
+          </h4>
+          <samll>
+            Best properties For sale in , compare with prices and photos between{" "}
+            <span className="counter">211,224</span> properties of different
+            areas. Search in using the map and contact the seller directly.
+          </samll>
+          <br />
+          <Card
+            style={{
+              width: 300,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+            actions={[
+              <SettingsOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              // <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+              title="Card title"
+              description="This is the description"
+            />
+          </Card>
         </Row>
       </Container>
     </>
